8b77cb6418b1e4a9f3beb6211eac2174a457b6d7
navratna vats <navratnavats@gmail.com>
2021-06-27 18:22:19 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,162 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		
+		<title>E-Society !! Home Page</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">=
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/nice-select.css">
+		    <link rel="stylesheet" href="css/ion.rangeSlider.css" />
+		    <link rel="stylesheet" href="css/ion.rangeSlider.skinFlat.css" />
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body>
+
+			<!-- Start Header Area -->
+			<header class="default-header">
+		
+
+				<nav class="navbar navbar-expand-lg  navbar-light bg-light">
+					<div class="container">
+						  <a class="navbar-brand" href="index.php">
+						  	<h2 style="color: brown">E-Society</h2>
+						  </a>
+						  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						    <span class="navbar-toggler-icon"></span>
+						  </button>
+
+						  <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
+						    <ul class="navbar-nav">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="admin/login.php">Admin</a></li>
+								<li><a href="user/login.php">Member</a></li>
+								
+						    </ul>
+						  </div>						
+					</div>
+				</nav>
+
+
+			</header>
+			<!-- End Header Area -->
+
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row  align-items-center justify-content-center" style="height: 32em;">
+						<div class="banner-content col-lg-12 col-md-12">
+							<h1 class="text-uppercase" >
+							E-Society
+							</h1>
+						
+						</div>
+					</div>
+				</div>
+				<!-- -->
+
+				<div class="container ">
+					<div class="card-deck">
+						<div class="card text-center" style="margin-bottom:4em";>
+							<div class="card-block">
+								<h5 class="card-header">Admin</h5>
+										<div class="card-body">
+											<h5 class="card-title"><strong> Admin/RWA Member Login </strong></h5>
+											<p class="card-text">We hope you have registered you society.</p>
+											<p class="card-text">Hello Admin. Please click below to Login</p>
+											<a href="admin/login.php" class="btn btn-primary">To Admin Page</a>
+										</div>
+							</div>
+						</div>
+
+						<div class="card text-center" style="margin-bottom:4em";>
+							<div class="card-block">
+								<h5 class="card-header">Members</h5>
+										<div class="card-body">
+											<h5 class="card-title"><strong>Society Member Login</strong></h5>
+											<p class="card-text">Welcome Society Memeber Here's your way to the account. Please click below for login</p>
+											<a href="user/login.php" class="btn btn-primary">Society Memeber Login</a>
+										</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- -->
+			</section>
+			<!-- End banner Area -->
+
+		
+				
+			<!-- Start city Area -->
+			<section class="city-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4 col-md-4 mb-10">
+						  <div class="content">
+						    <img class="content-image img-fluid d-block mx-auto" src="img/p1.jpg" alt="">
+						   
+						  </div>
+						</div>
+						<div class="col-lg-8 col-md-8 mb-10">
+							<div class="content">
+							   
+							  		 <img class="content-image img-fluid d-block mx-auto" src="img/p2.jpg" alt="">
+							     
+							    </a>
+						  	</div>
+							<div class="row city-bottom">
+								<div class="col-lg-6 col-md-6 mt-30">
+									<div class="content">
+									  
+									  		 <img class="content-image img-fluid d-block mx-auto" src="img/p3.jpg" alt="">
+									     
+									    </a>
+									</div>
+								</div>
+								<div class="col-lg-6 col-md-6 mt-30">
+									<div class="content">
+									   	 <img class="content-image img-fluid d-block mx-auto" src="img/p4.jpg" alt="">
+									   
+									</div>
+								</div>								
+							</div>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End city Area -->
+	
+
+			<!-- start footer Area -->		
+			<footer class="footer-area section-gap">
+				<div class="container">
+					
+					<div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						<h2 class="footer-text m-0" style="color: red">E-Society </h2>
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					</div>
+				</div>
+			</footer>	
+			<!-- End footer Area -->		
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.nice-select.min.js"></script>
+			<script src="js/jquery.sticky.js"></script>
+			<script src="js/ion.rangeSlider.js"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>
+			<script src="js/main.js"></script>	
+		</body>
+	</html>
---
+++ user/includes/sidebar.php
@@ -0,0 +1,38 @@
+<div id="leftsidebar" class="sidebar">
+        <div class="sidebar-scroll">
+            <nav id="leftsidebar-nav" class="sidebar-nav">
+                <ul id="main-menu" class="metismenu">
+                    <li class="heading">Main</li>
+                    <li><a href="dashboard.php"><i class="icon-home"></i><span>Dashboard</span></a></li>
+                    
+                    <li class="middle">
+                        <a href="view-bill.php"><i class="icon-diamond"></i><span>View Bill</span></a>
+                      
+                    </li>
+                  
+                   
+                    <li>
+                        <a href="view-visitorlist.php"><i class="icon-users"></i><span>Visitors</span></a>
+                        
+                    </li>
+                    <li>
+                        <a href="#Tables" class="has-arrow"><i class="icon-tag"></i><span>Complain</span></a>
+                        <ul>
+                            <li><a href="raise-complain.php">Raise Complain</a></li>
+                            <li><a href="complain-status.php">Complain Status</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="search-visitor.php"><i class="icon-magnifier"></i><span>Search</span></a>
+                       
+                    </li>
+                    <li>
+                        <a href="visitor-bwdates.php"><i class="icon-folder"></i><span>Report</span></a>
+                       
+                    </li>
+                                          
+                   
+                </ul>
+            </nav>
+        </div>
+    </div>
